82c9c1d90a981c159267e08c47a4da2d053c1efc
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-02-12 12:12:55 +1300

    Cart page: quantity  removed

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -11,21 +11,20 @@ function Cart (props) {
       <table>
         <thead>
           <tr>
-            <td>Animal</td>
             <td>Name</td>
             <td>Price</td>
             <td>Remove</td>
           </tr>
         </thead>
         <tbody>
-          {props.cart.map(({ id, name, price, quantity }) => {
+          {props.cart.map(({ id, name, price }) => {
             return (
               <tr key={id}>
                 <td>{name}</td>
-                <td><input className='update-input' value={quantity} /></td>
+                
                 <td>{price}</td>
                 {/* TODO: implement deletes */}
-                <td><button><span className='fa fa-trash fa-2x' /></button></td>
+                <td><button><span className='fa fa-trash fa-2x' />Delete</button></td>
               </tr>
             )
           })}
@@ -33,8 +32,7 @@ function Cart (props) {
       </table>
 
       <p className='actions'>
-        <a href='#'>Continue shopping</a>
-        <button>Update</button> {/* TODO: implement updates */}
+        <a href='#'>Continue shopping</a>      
         <button className='button-primary'>Checkout</button>
       </p>
       </div>
