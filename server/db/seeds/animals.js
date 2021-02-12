62cb3c03581dea15f0abc511c9d9f012849c8085
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-02-12 14:15:32 +1300

    add animals to db

--- server/db/seeds/animals.js
+++ server/db/seeds/animals.js
@@ -6,7 +6,11 @@ exports.seed = function (knex) {
       return knex('animals').insert([
         { id: 1, name: 'Boris', type: 'Elephant', price: 300, location: 'Africa'   },
         { id: 2, name: 'Maurice', type: 'Orangutan', price: 600, location: 'Asia'  },
-        { id: 3, name: 'Willy', type: 'Blue Whale', price: 5, location: 'Australasia'  }
+        { id: 3, name: 'Willy', type: 'Blue Whale', price: 5, location: 'Australasia'  },
+        { id: 4, name: 'Moley', type: 'Naked Mole Rat', price: 9000, location: 'Asia'  },
+        { id: 5, name: 'Barry', type: 'Sun Bear', price: 20, location: 'Europe'  },
+        { id: 6, name: 'Samuel', type: 'Sloth', price: 600, location: 'Africa'  },
+        { id: 7, name: 'Penelope', type: 'Python', price: 80000, location: 'Australasia'  }
       ])
     })
 }
